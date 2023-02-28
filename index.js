a8413b63e6f37c30ca09740c41e53a78d3081f62
lokeshvasnik <lokeshvasnik2003@gmail.com>
2023-02-28 11:02:43 +0530

    Ready

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const Project = require('./model/Links');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const URI =
-    'mongodb+srv://ProjectsLinks:c9LfXyalAfT1dQMw@projects.ifutfhs.mongodb.net/?retryWrites=true&w=majority';
+const URI = process.env.URL;
 const PORT = process.env.PORT || 3000;
+const morgan = require('morgan');
 
 // Middle ware
 app.use(express.json());
 app.use(cors());
+morgan('tiny');
 
 // Connection to db
 
@@ -39,20 +41,62 @@ app.get('/api', async (req, res) => {
 
 // Register route
 app.post('/api', async (req, res) => {
-    const { title, des, website, img } = req.body;
+    const { title, description, imageLink, githubLink, websiteLink, category } =
+        req.body;
     try {
-        const Projects = await Project.create({ title, des, website, img });
-        res.json(Projects);
+        await Project.create({
+            title,
+            description,
+            imageLink,
+            githubLink,
+            websiteLink,
+            category,
+        });
+        res.status(200).json({ message: 'SUCCESSFULLY DATA ADDED' });
     } catch (error) {
-        // log the error message
         console.log(error);
         res.status(400).json(error);
     }
 });
 
-// Listening to port
-app.listen(PORT, () => {
-    console.log(`Connect to port ${PORT}`);
+// Find Project by id
+app.get('/api/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const Projects = await Project.findById(id);
+        res.json(Projects);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'something went wrong' });
+    }
 });
 
-// c9LfXyalAfT1dQMw
+app.put('/api/:id', async (req, res) => {
+    try {
+        const result = await Project.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+});
+app.delete('/api/:id', async (req, res) => {
+    try {
+        const result = await Project.findByIdAndDelete(req.params.id);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+});
+
+// Listening to port
+app.listen(PORT, () => {
+    try {
+        console.log(`Connect to port ${PORT}`);
+    } catch (err) {
+        console.log(err);
+    }
+});
--- model/Links.js
+++ model/Links.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 const ProjectSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    des: { type: String, required: true },
-    website: { type: String, required: true },
-    img: { type: String, required: true },
+    description: { type: String, required: true },
+    imageLink: { type: String, required: true },
+    githubLink: { type: String, required: true },
+    websiteLink: { type: String, required: true },
+    category: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Project', ProjectSchema);
